8854a79e3ff89f944682ca3e41ecda349686b8f3
= <daniel20025febrero29@gmail.com>
2024-03-02 16:10:46 -0500

    Send email synce form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,79 +1,123 @@
-import { useState } from "react";
-import axios from "axios";
+import { useState } from "react"
+import axios from "axios"
 
 const ContactForm = () => {
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [firstName, setFirstName] = useState('')
+  const [secondName, setSecondName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [empyes, setEmpyes] = useState(false)
 
-  const onSubmit = (data) => {
-    debugger
-    sendEmail(data);
-  };
+  const onSubmit = () => {
+    if (firstName == '' || secondName == '' || email == '' || message == '') {
+      setEmpyes(true)
+      setTimeout(() => {
+        setEmpyes(false)
+      }, [5000])
+      return
+    }
+    sendEmail()
+  }
 
   const sendEmail = async () => {
-    alert('')
-    const result = await fetch(apiGmail, {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-
-    axios.post('https://send-email-porfolio.vercel.app/send/mail')
-      .then(res => {
-        setPosts(res.data);
+    axios.post('https://send-email-porfolio.vercel.app/send/mail',
+      {
+        "firstname": firstName,
+        "secondname": secondName,
+        "email": email,
+        "message": message
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
       });
+    setEmpyes(false)
   }
 
 
   return (
     <div className='w-full'>
-      <form onSubmit={onSubmit} className='flex flex-col gap-4'>
+      <div className='flex flex-col gap-4'>
         <div className='flex gap-4'>
           <div className='grow'>
             <input
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
               placeholder='First name'
               className='border border-zinc-200 rounded-full py-2 px-4 w-full'
             />
-            <span className='block py-1 h-6 text-xs text-red-500'>This field is required</span>
+            {
+              !empyes ? (
+                null
+              ) : (
+                <span className='block py-1 h-6 text-xs text-red-500'>{firstName == '' ? 'This field is required' : null}</span>
+              )
+            }
           </div>
 
           <div className='grow'>
             <input
+              value={secondName}
+              onChange={(event) => setSecondName(event.target.value)}
+              autoComplete='off'
               placeholder='Last name'
               className='border border-zinc-200 rounded-full py-2 px-4 w-full'
             />
-            <span className='block py-1 h-6 text-xs text-red-500'>This field is required</span>
+            {
+              !empyes ? (
+                null
+              ) : (
+                <span className='block py-1 h-6 text-xs text-red-500'>{secondName == '' ? 'This field is required' : null}</span>
+              )
+            }
           </div>
         </div>
         <div className='flex gap-4'>
           <div className='grow'>
             <input
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
               placeholder='you@email.com'
               className='border border-zinc-200 rounded-full py-2 px-4 w-full'
             />
-            <span className='block py-1 h-6 text-xs text-red-500'>This field is required</span>
+            {
+              !empyes ? (
+                null
+              ) : (
+                <span className='block py-1 h-6 text-xs text-red-500'>{email == '' ? 'This field is required' : null}</span>
+              )
+            }
           </div>
         </div>
         <div className='flex gap-4'>
           <div className='grow'>
             <textarea
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
               placeholder='Message'
               className='border border-zinc-200 rounded-full py-2 px-4 w-full'
             >
             </textarea>
-            <span className='block py-1 h-6 text-xs text-red-500'>This field is required</span>
+            {
+              !empyes ? (
+                null
+              ) : (
+                <span className='block py-1 h-6 text-xs text-red-500'>{message == '' ? 'This field is required' : null}</span>
+              )
+            }
           </div>
         </div>
         <div className='flex justify-end'>
-          <button type='submit' className='px-4 py-2 text-white font-medium rounded-full bg-gray-500'>
-            Submit
+          <button onClick={() => onSubmit()} className='px-4 py-2 text-white font-medium rounded-full bg-gray-500'>
+            Enviar
           </button>
         </div>
-      </form>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default ContactForm;
+export default ContactForm
