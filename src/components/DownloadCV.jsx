ea7471f0d4fd1d119e725277f41239dde88206d1
= <daniel20025febrero29@gmail.com>
2024-03-09 00:16:14 -0500

    Change name pdf

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -1,17 +1,17 @@
-import DownloadIcon from "../assets/icons/DownloadIcon.astro";
-import SocialIcons from "../components/SocialIcons.astro";
-import pdf from "../assets/docs/cv.pdf";
+import DownloadIcon from "../assets/icons/DownloadIcon.astro"
+import SocialIcons from "../components/SocialIcons.astro"
+import pdf from "../assets/docs/cv.pdf"
 
 function DownloadCv() {
     const handleDownloadCV = () => {
-        const pdfUrl = pdf;
-        const link = document.createElement("a");
-        link.href = pdfUrl;
-        link.download = "document.pdf"; // specify the filename
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
+        const pdfUrl = pdf
+        const link = document.createElement("a")
+        link.href = pdfUrl
+        link.download = "CV-Daniel-Muñoz.pdf"
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+    }
 
     return (
         <button className="flex justify-center items-center gap-0 sm:gap-2 h-full w-full" onClick={() => handleDownloadCV()} >
@@ -21,11 +21,11 @@ function DownloadCv() {
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
-                    stroke-width="1.5"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 ><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path
                     d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"></path><path
                         d="M7 11l5 5l5 -5"></path><path d="M12 4l0 12"></path></svg>
