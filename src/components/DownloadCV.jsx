8093f5498286082e723bfb8338c60991f621a2cd
= <daniel20025febrero29@gmail.com>
2024-05-31 20:33:50 -0500

    Add image and project

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -13,9 +13,9 @@ function DownloadCv() {
 
     return (
         <button className="flex justify-center items-center gap-0 sm:gap-2 h-full w-full" onClick={() => handleDownloadCV()} >
-            <div class="size-6">
+            <div className="size-6">
                 <svg
-                    class="icon icon-tabler icon-tabler-download flex justify-center items-center"
+                    className="icon icon-tabler icon-tabler-download flex justify-center items-center"
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
@@ -29,7 +29,7 @@ function DownloadCv() {
                         d="M7 11l5 5l5 -5"></path><path d="M12 4l0 12"></path></svg>
             </div>
             <div>
-                <span class="hidden sm:block">CV</span>
+                <span className="hidden sm:block">CV</span>
             </div>
         </button>
     )
